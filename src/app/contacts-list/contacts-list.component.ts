b67d3681c129a9e29c2b7d0ea51dfdf9bf4513e8
Markus Weser <git@markusweser.info>
2018-03-12 15:12:03 +0000

    feat: Exercise: Debounce and deduplicate terms https://github.com/thoughtram/angular-master-class-exercise-descriptions/blob/master/exercises/observables/exercise-2_debounce-distinctuntilchanged.md

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -3,6 +3,9 @@ import { Observable } from 'rxjs/Observable';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
 
+import { Subject } from 'rxjs/Subject'
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 @Component({
   selector: 'trm-contacts-list',
   templateUrl: './contacts-list.component.html',
@@ -12,10 +15,15 @@ export class ContactsListComponent implements OnInit {
 
   contacts$: Observable<Array<Contact>>;
 
+  terms$ = new Subject<string>();
+
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit () {
     this.contacts$ = this.contactsService.getContacts();
+    this.terms$.pipe(debounceTime(400), distinctUntilChanged()).subscribe(val => {
+      this.search(val);
+    })
   }
 
   trackByContactId(index, contact) {
