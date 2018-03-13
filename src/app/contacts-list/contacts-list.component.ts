f4b9c6ffb7ab4fd73d46dd0a4c986164368ec56e
Markus Weser <git@markusweser.info>
2018-03-13 11:57:02 +0000

    feat: Exercise: Move search logic into service https://github.com/thoughtram/angular-master-class-exercise-descriptions/blob/master/exercises/observables/exercise-4_move-logic-into-service.md

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -3,10 +3,9 @@ import { Observable } from 'rxjs/Observable';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
 
-import { Subject } from 'rxjs/Subject'
+import { Subject } from 'rxjs/Subject';
 
-// Import switchMap and merge just as we did with the other operators
-import { debounceTime, distinctUntilChanged, merge, switchMap } from 'rxjs/operators';
+import { merge } from 'rxjs/operators';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -22,12 +21,9 @@ export class ContactsListComponent implements OnInit {
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit () {
-    this.contacts$ = this.terms$.pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap(term => this.contactsService.search(term)),
-      merge(this.contactsService.getContacts()),
-    );
+    this.contacts$ = this.contactsService
+      .search(this.terms$)
+      .pipe(merge(this.contactsService.getContacts()));
   }
 
   trackByContactId(index, contact) {
